ce0dc7f3f10887cdd2a88395404377933cc4a25d
BayuYogi <pitaganakomangbayu@gmail.com>
2025-09-28 11:58:45 +0800

    menambahkan materi view

--- app/controllers/About.php
+++ app/controllers/About.php
@@ -1,12 +1,23 @@
 <?php
 
-class About{
-    public function index($nama='bayu',$mahasiswa='mahasiswa')
+class About extends Controller{
+    public function index($nama='bayu',$mahasiswa='mahasiswa',$umur=20)
     {
-        echo"hallo saya $nama saya adalah seorang $mahasiswa di pnb";
+        $data['nama']=$nama;
+        $data['mahasiswa']=$mahasiswa;
+        $data['umur']=$umur;
+        $data['judul']='About me';
+        $this->view('templates/header', $data);
+        $this->view('about/index',$data);
+        $this->view('templates/footer');
+
     }
     public function page()
     {
-        echo'About/page';
+        $data['judul']='pages';
+        $this->view('templates/header', $data);
+        $this->view('about/page');
+        $this->view('templates/footer');
+
     }
 }
---
+++ app/views/about/index.php
@@ -0,0 +1,3 @@
+
+    <h1>About me</h1>
+    <p>hallo nama saya <?=$data['nama']?>,umur saya <?=$data['umur']?>,saya adalah seorang <?=$data['mahasiswa']?> di pnb</p>
