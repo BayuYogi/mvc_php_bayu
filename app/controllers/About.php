ae9f5760b82f33ad5328387dae9acc13f865ee7a
BayuYogi <pitaganakomangbayu@gmail.com>
2025-09-28 09:49:27 +0800

    first commit

---
+++ app/controllers/About.php
@@ -0,0 +1,12 @@
+<?php
+
+class About{
+    public function index($nama='bayu',$mahasiswa='mahasiswa')
+    {
+        echo"hallo saya $nama saya adalah seorang $mahasiswa di pnb";
+    }
+    public function page()
+    {
+        echo'About/page';
+    }
+}
