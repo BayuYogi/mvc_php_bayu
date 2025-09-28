32f5d795aca64355e75f91b4b48a4e9eb70d75d5
BayuYogi <pitaganakomangbayu@gmail.com>
2025-09-28 18:39:11 +0800

    menambahkan materi asset

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,3 +1,6 @@
-
-    <h1>About me</h1>
+<div class="container">
+    <h1 class mt-4>About me</h1>
+     <img src="<?= BASEURL;?>/img/bayganteng.jpg" alt="bayu yogi" width="200" class="rounded-circle shadow">
     <p>hallo nama saya <?=$data['nama']?>,umur saya <?=$data['umur']?>,saya adalah seorang <?=$data['mahasiswa']?> di pnb</p>
+   
+</div>
